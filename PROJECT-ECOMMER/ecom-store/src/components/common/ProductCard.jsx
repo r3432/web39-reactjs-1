1c41ab0feaa0eca2c7498c70b996c534f95f96f3
r3432 <duonglinh181200@gmail.com>
2024-07-31 10:41:23 +0700

    up

---
+++ PROJECT-ECOMMER/ecom-store/src/components/common/ProductCard.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const ProductCard = ({product}) => {
+
+
+  const imgSrc=product.image;
+const imgAlt=product.title;
+
+
+  return (
+    <div className="card h-100">
+              {/* Sale badge*/}
+              <div
+                className="badge bg-dark text-white position-absolute"
+                style={{ top: "0.5rem", right: "0.5rem" }}
+              >
+                Sale
+              </div>
+              {/* Product image*/}
+              <img
+                className="card-img-top"
+                src={imgSrc}
+                alt={imgAlt}
+              />
+              {/* Product details*/}
+              <div className="card-body p-4">
+                <div className="text-center">
+                  {/* Product name*/}
+                  <h5 className="fw-bolder">{product.title}</h5>
+                  {/* Product reviews*/}
+                  <div className="d-flex justify-content-center small text-warning mb-2">
+                    <div className="bi-star-fill" />
+                    <div className="bi-star-fill" />
+                    <div className="bi-star-fill" />
+                    <div className="bi-star-fill" />
+                    <div className="bi-star-fill" />
+                  </div>
+                  {/* Product price*/}
+                  <span className="text-muted text-decoration-line-through">
+                    $20.00
+                  </span>
+                  $18.00
+                </div>
+              </div>
+              {/* Product actions*/}
+              <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div className="text-center">
+                  <a className="btn btn-outline-dark mt-auto" href="#">
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+          
+          
+          
+          
+            </div>
+  )
+}
+
+export default ProductCard
